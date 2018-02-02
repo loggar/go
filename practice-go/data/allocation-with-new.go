d1497d313f5bc2321a025ceecfd755c29c2540de
charly.lee <webnl@DT-Charly.koi.local>
2018-02-02 14:34:07 +1100

    data allocation with new

---
+++ practice-go/data/allocation-with-new.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"sync"
+)
+
+/*
+SyncedBuffer ...
+*/
+type SyncedBuffer struct {
+	lock   sync.Mutex
+	buffer bytes.Buffer
+}
+
+func main() {
+	p := new(SyncedBuffer) // type *SyncedBuffer
+	var v SyncedBuffer     // type  SyncedBuffer
+
+	fmt.Println(p)
+	fmt.Println(v)
+}
