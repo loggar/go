4d4ad86658c4ed15ad55a52e997b6e305f5cfd3b
charly.lee <webnl@DT-Charly.koi.local>
2018-06-08 11:10:19 +1000

    practice-go-web

--- practice-web-application/concurrent-http-request/sequential.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-print(len(requests.get('http://localhost:8080/param1').text))
-print(len(requests.get('http://localhost:8080/param2').text))
-print(len(requests.get('http://localhost:8080/param3').text))
-
-## python sequential.py
