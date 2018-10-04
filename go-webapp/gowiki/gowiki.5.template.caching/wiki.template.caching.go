fcf87bc8d68245db392761edb8f4d3c55ab92734
charly.lee <webnl@DT-Charly.koi.local>
2018-10-04 11:32:37 +1000

    refactor (data) dist

--- go-webapp/gowiki/gowiki.5.template.caching/wiki.template.caching.go
+++ go-webapp/gowiki/gowiki.5.template.caching/wiki.template.caching.go
@@ -11,7 +11,7 @@ import (
 
 var gopath = os.Getenv("GOPATH")
 var rootDir = "/src/github.com/loggar/go"
-var saveDir = "/_tmp/practice-go-web/gowiki/"
+var saveDir = "/_data/practice-go-web/gowiki/"
 
 var templates = template.Must(template.ParseFiles("edit.html", "view.html"))
 
