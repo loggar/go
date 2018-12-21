5305de7cfcd2464e029f2d1e852cad655c168669
charly.lee <webnl@DT-Charly.koi.local>
2018-12-21 16:55:59 +1100

    feat (json, error-handling)

---
+++ go-packages/json/decode.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type response struct {
+	PageNumber int      `json:"page"`
+	Fruits     []string `json:"fruits"`
+}
+
+func testMain() {
+	str := `{"page": 1, "fruits": ["apple", "peach"]}`
+	res := response{}
+	json.Unmarshal([]byte(str), &res)
+	fmt.Println(res.PageNumber)
+}
+
+//=> 1
