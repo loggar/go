170a75c3c2903bb6b40bf38e8daffcf06433987e
loggar <charly.loggar@gmail.com>
2022-09-17 21:13:03 +1000

    feat (var) const

---
+++ go-core/print/printf.expression.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+func main() {
+	num := 3
+	fmt.Printf("%v %T", num, num)
+}
+
+/*
+Format expression
+%t bool
+%b binary int
+%c char
+%d decimal number
+%o oct number
+%x hexa number
+%f floating number
+%e e number
+%s string
+%p pointer
+%U unicode
+%T variable type
+%v all
+%#v delimiter #
+*/
---
+++ go-core/variables/const.2.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+const (
+	c1 = 10     // first one must be assigned
+	c2          // set to 10
+	c3          // set to 10
+	c4 = "here" // different type
+	c5          // set to "here"
+	c6 = iota   // index will be assigned as value (until c8, until assignment is skipped)
+	c7
+	c8
+	c9 = "earth"
+	c10
+	c11 = "End"
+)
+
+func main() {
+	fmt.Println(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)
+}
+
+//  10 10 10 here here 5 6 7 earth earth End
