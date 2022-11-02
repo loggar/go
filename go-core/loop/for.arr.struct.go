6d2465a1269e3adfc0fcc537f4ad66465f5f51e6
loggar <charly.loggar@gmail.com>
2022-11-03 01:06:53 +1100

    feat (struct) loop

---
+++ go-core/loop/for.arr.struct.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+type Book struct {
+	Id   int64
+	Name string
+}
+
+func main() {
+	list := []*Book{
+		{
+			Id:   123,
+			Name: "name123",
+		},
+
+		{
+			Id:   124,
+			Name: "name124",
+		},
+	}
+
+	fmt.Printf("list (%T): %v\n", list, list)
+
+	var book *Book
+	for i, b := range list {
+		fmt.Println(i, b)
+		if b.Id == 124 {
+			book = b
+		}
+	}
+
+	fmt.Printf("book: %v", book)
+}
