9ea6487361e93689f43f8d310d6f533fcfa56e32
Charly LEE <charly.loggar@gmail.com>
2024-04-19 18:36:34 +1000

    feat (test) logf in testing

--- go-testing/testing-in-go/testing.1.basics_test.go
+++ go-testing/testing-in-go/testing.1.basics_test.go
@@ -3,11 +3,24 @@ package basics
 import (
 	"fmt"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestLoginTesting(t *testing.T) {
 	fmt.Println("1. This will be displayed when running tests")
 	t.Log("2. This will be displayed when running tests")
+}
+
+func TestLogfInTesting(t *testing.T) {
+	expected := "expected"
+	actual := "actual"
+
+	// Use assert.Equal to check equality; it returns a boolean status.
+	if !assert.Equal(t, expected, actual, "Output") {
+		t.Logf("Expected: %q", expected)
+		t.Logf("Actual: %q", actual)
+	}
 }
 
 func TestTruth(t *testing.T) {
